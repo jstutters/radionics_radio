e24f05fa693c829138401562c81dfee26ef923ac
Jon Stutters <jstutters@jeremah.co.uk>
2014-09-08 15:53:49 +0100

    Added php to email frequency values

---
+++ contact-form-handler.php
@@ -0,0 +1,23 @@
+<?php
+
+$errors = '';
+$myemail = 'jstutters@jeremah.co.uk';
+if(empty($_POST['username']) || empty($_POST['thought']) || empty($_POST['frequencies'])) {
+  $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['username'];
+$email_address = $_POST['thought'];
+$message = $_POST['frequencies'];
+if(empty($errors))
+{
+  $to = $myemail;
+  $email_subject = "$name";
+  $email_body = "You have received a new message. ".
+    "\n Username: $name\nThought: $thought\nFrequencies:\n$message";
+  $headers = "From: $myemail\n";
+  mail($to, $email_subject, $email_body, $headers);
+  //redirect to the 'thank you' page
+  echo $to;
+  echo $email_body;
+}
+?>
