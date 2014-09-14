698f6621b4065246db4adc8a450e1c50ed9759b8
Jonny Stutters <jstutters@jeremah.co.uk>
2014-09-14 10:14:52 +0100

    Fixed contact form handler (was using wrong variable names)

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -5,18 +5,15 @@ if(empty($_POST['username']) || empty($_POST['thought']) || empty($_POST['freque
   $errors .= "\n Error: all fields are required";
 }
 $name = $_POST['username'];
-$email_address = $_POST['thought'];
+$thought = $_POST['thought'];
 $message = $_POST['frequencies'];
 if(empty($errors))
 {
   $to = $myemail;
-  $email_subject = "$name";
+  $email_subject = "Radionics frequencies from $name";
   $email_body = "You have received a new message. ".
     "\n Username: $name\nThought: $thought\nFrequencies:\n$message";
   $headers = "From: $myemail\n";
   mail($to, $email_subject, $email_body, $headers);
-  //redirect to the 'thank you' page
-  echo $to;
-  echo $email_body;
 }
 ?>
